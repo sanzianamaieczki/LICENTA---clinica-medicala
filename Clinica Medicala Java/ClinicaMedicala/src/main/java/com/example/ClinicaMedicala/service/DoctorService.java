873aadc2a3b5997de25b0a08fdbafc570a5571dc
Sinziana Maieczki <petronela-sinziana.maieczki@student.tuiasi.ro>
2025-02-13 13:54:55 +0200

    verificari complete doctor service

--- Clinica Medicala Java/ClinicaMedicala/src/main/java/com/example/ClinicaMedicala/service/DoctorService.java
+++ Clinica Medicala Java/ClinicaMedicala/src/main/java/com/example/ClinicaMedicala/service/DoctorService.java
@@ -35,7 +35,8 @@ public class DoctorService {
     @Autowired
     private SpecializationRepository specializationRepository;
 
-//    private CheckFields checkFields = new CheckFields();
+    //lista doctorilor existenti
+    List<Doctor> existingDoctors = doctorRepository.findDoctorsByFilters(null, null,null,null,null);
 
     public List<DoctorDTO> getDoctorsByFilters(
             Boolean is_deleted,
@@ -72,16 +73,33 @@ public class DoctorService {
         StringBuilder errors = new StringBuilder();
 
         //verificare daca datele introduse sunt nule
-        Map<String, Object> doctorFields = DTOConverter.convertToMap(doctorDTO);
-        errors.append(CheckFields.checkEmptyFields(doctorFields,Set.of("id_doctor")));
+        errors.append(CheckFields.checkEmptyFields(
+                DTOConverter.convertToMap(doctorDTO),
+                Set.of("id_doctor")))
+                .append(System.lineSeparator());
+
+        //verificare daca exista clinica cu id-ul primit
+        Clinic clinic = clinicRepository.findById(doctorDTO.getId_clinic()).orElse(null);
+        if(clinic == null) {
+            errors.append("Clinica cu id-ul: ").append(doctorDTO.getId_clinic()).append(" nu exista.")
+                    .append(System.lineSeparator());
+        }
+
+        //verificare daca exista specializarea cu id-ul primit
+        Specialization specialization =specializationRepository.findById(doctorDTO.getId_specialization()).orElse(null);
+        if(specialization == null) {
+            errors.append("Specializarea cu id-ul ").append(doctorDTO.getId_specialization()).append(" nu exista.")
+                    .append(System.lineSeparator());
+        }
 
         //verificare daca datele introduse nu exista deja
-        List<Doctor> existingDoctors = doctorRepository.findDoctorsByFilters(null, null,null,null,null);
         if(existingDoctors.stream().anyMatch(d -> d.getEmail().equals(doctorDTO.getEmail()))) {
-            errors.append("Exista deja acest email: ").append(doctorDTO.getEmail()).append(System.lineSeparator());
+            errors.append("Exista deja acest email: ").append(doctorDTO.getEmail())
+                    .append(System.lineSeparator());
         }
         if(existingDoctors.stream().anyMatch(d -> d.getPhone().equals(doctorDTO.getPhone()))) {
-            errors.append("Exista deja acest numar de telefon: ").append(doctorDTO.getPhone()).append(System.lineSeparator());
+            errors.append("Exista deja acest numar de telefon: ").append(doctorDTO.getPhone())
+                    .append(System.lineSeparator());
         }
 
         //afisarea erorilor
@@ -90,13 +108,6 @@ public class DoctorService {
         }
 
         Doctor doctor = new Doctor(doctorDTO);
-
-        Clinic clinic = clinicRepository.findById(doctorDTO.getId_clinic())
-                .orElseThrow(() -> new IllegalArgumentException("Clinica cu id-ul " + doctorDTO.getId_clinic() + " nu exista."));
-
-        Specialization specialization = specializationRepository.findById(doctorDTO.getId_specialization())
-                .orElseThrow(() -> new IllegalArgumentException("Specializarea cu id-ul " + doctorDTO.getId_specialization() + " nu exista."));
-
         doctor.setClinic(clinic);
         doctor.setSpecialization(specialization);
         doctor.setCreated_at(new Date());
@@ -112,19 +123,12 @@ public class DoctorService {
         //verificari necesare
         StringBuilder errors = new StringBuilder();
 
-        //verificare daca exista doctorul mentionat
+        //verificare daca exista doctorul mentionat - daca nu exista, nu mai continuam verificarile
         Doctor doctor = doctorRepository.findDoctorById(id_doctor)
                 .orElseThrow(() -> new IllegalArgumentException("Nu a fost gasit doctorul cu id-ul: " + id_doctor));
 
         //verificare daca nu se introduc date nule
         errors.append(CheckFields.checkEmptyFields(updates, Set.of("id_doctor")));
-
-        //afisarea erorilor
-        if(!errors.isEmpty()) {
-            throw new IllegalArgumentException(errors.toString().trim());
-        }
-
-        //to do: verificare daca nu cumva exista acel nume
 
         updates.forEach((field,value) ->{
             switch (field){
@@ -135,30 +139,50 @@ public class DoctorService {
                     doctor.setLast_name((String) value);
                     break;
                 case "email":
+                    if(existingDoctors.stream().anyMatch(d -> d.getEmail().equals(value))) {
+                        errors.append("Exista deja acest email: ").append(value).append(System.lineSeparator());
+                    }
                     doctor.setEmail((String) value);
                     break;
+                case "phone":
+                    if(existingDoctors.stream().anyMatch(d -> d.getPhone().equals(value))) {
+                        errors.append("Exista deja acest numar de telefon: ").append(value).append(System.lineSeparator());
+                    }
+                    doctor.setPhone((String) value);
                 case "id_clinic":
                     Integer id_clinic = (Integer) value;
-                    Clinic clinic = clinicRepository.findClinicById(id_clinic)
-                            .orElseThrow(()->new IllegalArgumentException("Nu a fost gasita clinica cu id-ul: " + id_clinic));
+                    Clinic clinic = clinicRepository.findClinicById(id_clinic).orElse(null);
+                    if(clinic == null) {
+                        errors.append("Nu a fost gasita clinica cu id-ul: ").append(id_clinic)
+                                .append(System.lineSeparator());
+                    }
                     doctor.setClinic(clinic);
                     break;
                 case "id_specialization":
                     Integer id_specialization = (Integer) value;
-                    Specialization specialization = specializationRepository.findSpecializationById(id_specialization)
-//                            .orElse(errors.append("Nu a fost gasita specializarea cu id-ul: ").append(id_specializati)
-                            .orElseThrow(()->new IllegalArgumentException("Nu a fost gasita specializarea cu id-ul: " + id_specialization));
+                    Specialization specialization = specializationRepository.findSpecializationById(id_specialization).orElse(null);
+                    if(specialization == null) {
+                        errors.append("Nu a fost gasita specializarea cu id-ul: ").append(id_specialization)
+                                .append(System.lineSeparator());
+                    }
                     doctor.setSpecialization(specialization);
                     break;
                 case "id_doctor":
                 case "created_at":
                 case "updated_at":
                 case "is_deleted":
-                    throw new IllegalArgumentException("Acest camp nu poate fi modificat: " + field);
+                    errors.append("Acest camp nu poate fi modificat").append(field)
+                            .append(System.lineSeparator());
                 default:
-                    throw new IllegalArgumentException("Acest camp nu exista: " + field);
+                    errors.append("Acest camp nu exista: ").append(field)
+                            .append(System.lineSeparator());
             }
         });
+
+        //afisarea erorilor
+        if(!errors.isEmpty()) {
+            throw new IllegalArgumentException(errors.toString().trim());
+        }
 
         doctor.setUpdated_at(new Date());
         Doctor savedDoctor = doctorRepository.save(doctor);
